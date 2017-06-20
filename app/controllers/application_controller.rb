b096842f9a643655884e57604d67e37d93c2ec37
ZXChadd <zhexianchadd@gmail.com>
2017-06-20 23:00:31 +0800

    Redirect users to edit profile after signing up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_up_path_for(resource)
+  protected
+  def after_inactive_sign_up_path_for(resource)
     edit_user_registration_path
   end
-
-  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :email, :provider, :uid ])
