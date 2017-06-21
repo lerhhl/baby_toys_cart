c60310e7a90a259b193b4879114cc731a9001ed6
ZXChadd <zhexianchadd@gmail.com>
2017-06-21 11:42:30 +0800

    Remove unnecessary code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  def after_inactive_sign_up_path_for(resource)
-    edit_user_registration_path
-  end
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :email, :provider, :uid ])
   end
