360d7a73db7c3a50efb692ebb66d5bd120377756
ZXChadd <zhexianchadd@gmail.com>
2017-06-27 10:04:54 +0800

    Add CSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,57 +9,31 @@ class ApplicationController < ActionController::Base
   end
 
   def current_cart
-    cart = Cart.find_by(session[:cart_id])
-    rescue ActiveRecord::RecordNotFound
-    if !cart && !current_user
-      cart = Cart.create
-      session[:cart_id] = cart.id
+    if current_user
+      user_cart = Cart.find_by(user_id: current_user.id)
     end
-    if cart && current_user && cart.user_id.nil?
-      cart.update(user_id: current_user.id)
+      session_cart = Cart.find(session[:cart_id])
+      rescue ActiveRecord::RecordNotFound
+
+
+    if !current_user && !session_cart
+      session_cart = Cart.create!
+      session[:cart_id] = session_cart.id
+
+    elsif !current_user && session_cart
+      session_cart
+
+    elsif current_user && user_cart
+      user_cart
+
+    elsif current_user && session_cart
+      session_cart.update(user_id: current_user.id)
+      user_cart = session_cart
+
+    else
+      cart = Cart.create(user_id: current_user.id)
       cart = Cart.find_by(user_id: current_user.id)
     end
-    cart
-  end
-
-  # def current_cart
-  #   @cart = Cart.find(session[:cart_id])
-  #   rescue ActiveRecord::RecordNotFound
-  #   if current_user
-  #     cart = Cart.create
-  #     cart.update_attributes(user_id: current_user.id)
-  #   else
-  #     Cart.find(session[:cart_id])
-  #     rescue ActiveRecord::RecordNotFound
-  #     cart = Cart.create
-  #     session[:cart_id] = cart.id
-  #   end
-  # end
-
-  # def set_cart
-  #   if current_user
-  #     @user_cart = ShoppingCart.find_by(user_id: current_user.id)
-  #   end
-  #   @session_cart = ShoppingCart.find_by(session_id: session[:session_id])
-  #   #returning visitor
-  #   if current_user && @session_cart
-  #     byebug
-  #     cart = ShoppingCart.find(@session_cart.id)
-  #     cart.update_attributes(user_id: current_user.id)
-  #     cart.update_attributes(session_id: "")
-  #     @cart = cart
-  #   elsif current_user && !@session_cart
-  #     byebug
-  #     @cart = ShoppingCart.create(user_id: current_user.id)
-  #     @cart = cart
-  #   elsif !current_user && !@session_cart
-  #     @cart = ShoppingCart.create(session_id: session[:session_id])
-  #   elsif !current_user && @session_cart
-  #     byebug
-  #     @cart = ShoppingCart.create(session_id: session[:session_id])
-  #     cart = ShoppingCart.find(@session_cart.id)
-  #     @cart = cart
-  #   end
-
+  end 
 
 end
