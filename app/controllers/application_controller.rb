dab3a8850baf92fa1008d5a148687035cc0bb4f7
ZXChadd <zhexianchadd@gmail.com>
2017-06-27 17:36:20 +0800

    Finalise Shopping Cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,23 +10,28 @@ class ApplicationController < ActionController::Base
   end
 
   def current_cart
-    if current_user && Cart.find_by(user_id: current_user.id) == nil
-      user_cart = Cart.create(user_id: current_user.id)
-    elsif current_user
-      if !session[:cart_id] && Cart.find_by(user_id: current_user.id) == nil
-        user_cart = Cart.create(user_id: current_user.id)
-      else
-        user_cart = Cart.find_by(user_id: current_user.id)
-        @cartlists = CartList.where(cart_id: session[:cart_id])
-        @cartlists.each do |cartlist|
-          cartlist.update(cart_id: user_cart.id)
-        end
-        session[:cart_id] = nil
-        user_cart
+    # if current_user && Cart.find_by(user_id: current_user.id) == nil
+    #   user_cart = Cart.create(user_id: current_user.id)
+    # if current_user
+      # if !session[:cart_id] && Cart.find_by(user_id: current_user.id) == nil
+      #   user_cart = Cart.create(user_id: current_user.id)
+      # else
+    if current_user && session[:cart_id]
+      user_cart = Cart.find_by(user_id: current_user.id)
+      @cartlists = CartList.where(cart_id: session[:cart_id])
+      @cartlists.each do |cartlist|
+        cartlist.update(cart_id: user_cart.id)
       end
+      session[:cart_id] = nil
+      user_cart
+
+    elsif current_user && !session[:cart_id]
+      user_cart = Cart.find_by(user_id: current_user.id)
+
     elsif session[:cart_id] == nil
       session_cart = Cart.create!
       session[:cart_id] = session_cart.id
+
     else
       session_cart = Cart.find(session[:cart_id])
     end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,10 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You need to implement the method below in your model (e.g. app/models/user.rb)
   @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
+      Cart.create(user_id: @user.id) if !Cart.find_by(user_id: @user.id)
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
+      Cart.create(user_id: @user.id)
       redirect_to new_user_registration_url
     end
   end
