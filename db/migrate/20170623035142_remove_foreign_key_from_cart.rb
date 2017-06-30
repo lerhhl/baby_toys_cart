65b7729987b91326fef63d530fc41aa7d71181cd
lerhhl <lerhhl@gmail.com>
2017-06-30 10:38:10 +0800

    Migration files

--- db/migrate/20170623035142_remove_foreign_key_from_cart.rb
+++
@@ -1,5 +0,0 @@
-class RemoveForeignKeyFromCart < ActiveRecord::Migration[5.1]
-  def change
-    remove_foreign_key :carts, column: :user_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20170623135554) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "order_products", force: :cascade do |t|
